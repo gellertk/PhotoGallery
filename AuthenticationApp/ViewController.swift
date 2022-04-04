30632441aa1024ecb4b2fa20a2175895d2e70b6d
Kirill Gellert <Gerry9@bk.ru>
2022-04-04 19:21:07 +0300

    Initial Commit

---
+++ AuthenticationApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AuthenticationApp
+//
+//  Created by Кирилл  Геллерт on 04.04.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
